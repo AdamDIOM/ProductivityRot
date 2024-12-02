b74d7f3edcfd5d5633d9df27ade76b03e12ff8c8
Adam Drummond <adamdrummond@dyn077006.shef.ac.uk>
2024-12-02 01:48:51 +0000

    added chill guy

--- src/index.js
+++ src/index.js
@@ -10,10 +10,11 @@ ReactDOM.render(
         <Col md={2} xs={12} className='d-none d-md-block subway-img-wrapper'>
           <img src="https://i.giphy.com/dkUtjuBEdICST5zG7p.webp" alt="subway surfer content" className='subway-img sil mx-auto'></img>
         </Col>
-        <Col md={8} xs={12} className='app-wrapper d-flex align-items-center'>
+        <Col md={{span: 8, offset: 2}} xs={12} className='app-wrapper d-flex flex-column align-items-center'>
           <App />
+          <img src="/skibidi chill guy.png" alt="chill guy wearing skibidi rizz bomb tshirt. Only £1!" className='skibidi-img' />
         </Col>
-        <Col md={2} xs={12} className='d-none d-md-block subway-img-wrapper'>
+        <Col md={{span: 2, offset: 10}} xs={12} className='d-none d-md-block subway-img-wrapper '>
           <img src="https://i.makeagif.com/media/7-02-2023/4DRkyD.gif" alt="subway surfer content" className='subway-img sir mx-auto'></img>
         </Col>
       </Row>
